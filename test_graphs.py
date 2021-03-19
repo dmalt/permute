f91f7523a0b69f558c5176d2776f25e7c09c95a4
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-03-19 19:39:06 +0300

    Implement tests for connected components

--- test_graphs.py
+++ test_graphs.py
@@ -1,10 +1,11 @@
 from hypothesis import given, settings, assume
 from hypothesis.strategies import integers, lists, tuples
-from pytest import raises
+from pytest import raises, fixture
 
-from graphs import Graph
+from graphs import Graph, CC
 
 
+# ------------------------- test Graph  ------------------------- #
 MAX_VERTS = 1e5
 
 def graph_sizes():
@@ -45,3 +46,36 @@ def test_add_edge_sets_adjacency(n_verts, v, w):
     g.add_edge(v, w)
     assert w in g.adj(v)
     assert v in g.adj(w)
+
+# --------------------------------------------------------------- #
+
+
+# --------------------------- test CC --------------------------- #
+@fixture
+def sample_graph():
+    """Create graph with two connected components"""
+    g = Graph(6)
+
+    g.add_edge(0, 1)
+    g.add_edge(1, 2)
+    g.add_edge(2, 0)
+
+    g.add_edge(3, 4)
+    g.add_edge(4, 5)
+    return g
+
+def test_number_of_components_correct(sample_graph):
+    cc = CC(sample_graph)
+    assert cc.count() == 2
+
+def test_vertices_get_correct_cc_ids(sample_graph):
+    cc = CC(sample_graph)
+
+    assert cc.id(0) == 0
+    assert cc.id(1) == 0
+    assert cc.id(2) == 0
+    assert cc.id(3) == 1
+    assert cc.id(4) == 1
+    assert cc.id(5) == 1
+
+# --------------------------------------------------------------- #
