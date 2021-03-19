6a69995349ad425a14311dfac6085ddfb0f4f5fe
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-03-19 18:07:47 +0300

    Add typing and add details to docstrings

--- graphs.py
+++ graphs.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Graph:
     """Adjacency-list graph representation
 
@@ -14,14 +17,14 @@ class Graph:
         self._E = 0
         self._adj = [[] for _ in range(n_verts)]
 
-    def __str__(self):
+    def __str__(self) -> str:
         res = []
         for i, neighbors in enumerate(self._adj):
             for j in neighbors:
                 res.append(f"{i} <--> {j}")
         return "\n".join(res)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Graph(n_verts={self._V})"
 
     def V(self):
@@ -30,9 +33,14 @@ class Graph:
         """
         return self._V
 
-    def E(self):
+    def E(self) -> int:
         """
         Return the number of edges
+
+        Returns
+        -------
+        int
+            Number of vertices
         """
         return self._E
 
@@ -43,20 +51,29 @@ class Graph:
         Parameters
         ----------
         v : int
+            First vertex of an edge
         w : int
+            Second vertex of an edge
 
         """
         self._adj[v].append(w)
         self._adj[w].append(v)
         self._E += 1
 
-    def adj(self, v: int):
+    def adj(self, v: int) -> List[int]:
         """
         Get neighbors of vertex v
 
         Parameters
         ----------
         v : int
+            Vertex index
+
+        Returns
+        -------
+        list
+            Neighbors of vertex v
+
         """
         return self._adj[v]
 
