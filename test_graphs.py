7c55536aa84f8f60491ef744ed1926d23a1cc9c3
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-03-19 17:56:55 +0300

    Refactor tests

--- test_graphs.py
+++ test_graphs.py
@@ -1,19 +1,29 @@
-from hypothesis import given, settings
-from hypothesis.strategies import integers
+from hypothesis import given, settings, assume
+from hypothesis.strategies import integers, lists, tuples
 from pytest import raises
 
 from graphs import Graph
 
 
-@settings(deadline=500)
-@given(integers(min_value=1, max_value=5e5))
+MAX_VERTS = 1e5
+
+def graph_sizes():
+    return integers(min_value=1, max_value=MAX_VERTS)
+
+def vertices():
+    return integers(min_value=0, max_value=MAX_VERTS - 1)
+
+def edges():
+    return tuples(vertices(), vertices())
+
+
+@given(graph_sizes())
 def test_normal_graph_creation(n_verts):
     g = Graph(n_verts)
     assert g.V() == n_verts
 
 
-@settings(deadline=500)
-@given(integers(min_value=-5e5, max_value=0))
+@given(integers(min_value=-MAX_VERTS, max_value=0))
 def test_creation_raises_value_error_for_negative_input(n_verts):
     with raises(ValueError):
         g = Graph(n_verts)
