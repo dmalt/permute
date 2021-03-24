89f9d1e58b93fb99bc5093c6b0bbcffe1c3ba3b6
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-03-25 01:20:55 +0300

    Implement computation of cluster-level statistic

--- stats.py
+++ stats.py
@@ -1,1 +1,21 @@
 """Statistical utils"""
+def compute_cluster_level_stats(stat_map, clusters):
+    """
+    Compute cluster-level statistic.
+
+    Parameters
+    ----------
+    stat_map: ndarray
+        Statistics computed for each point, i.e. for each time and space
+    clusters: list of tuple of ndarray
+        Clusters for which cluster-level statisitc is to be computed.
+        len of tuple for each cluster should equal stat_map.ndim
+
+    Returns
+    -------
+    cluster_stats: list of floats
+        Cluster-level statistics
+
+    """
+
+    return [stat_map[cluster].sum() for cluster in clusters]
